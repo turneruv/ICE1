df43baf863c3f0885084a8b320ab7ab989761897
turneruv <turneruv@sheridancollege.ca>
2021-02-06 13:34:58 -0500

    Ruvon Turner: second

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -1,14 +1,13 @@
-
+/**
+ * @author Ruvon Turner
+ * Student number: 991623745
+ */
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
-/**
- *
- * @author Ronak
- */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card c1=new Card();
     }
     
 }
